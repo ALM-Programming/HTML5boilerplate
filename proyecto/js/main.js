4a863b073cf2158eba7e7e0d740926814ab7411f
Antonio <ALM-programming@hotmail.com>
2023-11-19 20:48:43 +0100

    correccion js

---
+++ proyecto/js/main.js
@@ -0,0 +1,4 @@
+const botonVerContenido = document.getElementById('ver-contenido')
+const botonVerHome = document.getElementById('ver-home')
+const botonVerEstilos = document.getElementById('ver-estilos')
+const botonVerAtributos = document.getElementById('ver-atributos')
